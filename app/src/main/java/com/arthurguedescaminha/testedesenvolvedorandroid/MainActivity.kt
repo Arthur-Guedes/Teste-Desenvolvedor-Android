db4992feb3e3cb6311fc453726e1e4fd8b9251e8
Arthur Guedes Caminha <arthurgc91@gmail.com>
2023-11-10 17:45:22 -0300

    Testa leitura dos dados pela API

--- app/src/main/java/com/arthurguedescaminha/testedesenvolvedorandroid/MainActivity.kt
+++ app/src/main/java/com/arthurguedescaminha/testedesenvolvedorandroid/MainActivity.kt
@@ -2,10 +2,26 @@ package com.arthurguedescaminha.testedesenvolvedorandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.gson.JsonObject
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        ApiService.service.listCards().enqueue(object : Callback<JsonObject> {
+            override fun onFailure(call: Call<JsonObject>, t: Throwable) {
+                // TODO t.message
+            }
+
+            override fun onResponse(call: Call<JsonObject>, response: Response<JsonObject>) {
+                // TODO response.body()!!["data"].asJsonArray
+            }
+        })
     }
+
 }
