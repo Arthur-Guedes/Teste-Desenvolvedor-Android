af0b2cc6d1af13f9ff99595bdf86ccbee74fe90c
Arthur Guedes Caminha <arthurgc91@gmail.com>
2023-11-10 22:10:59 -0300

    Adiciona eventos de verificação da barra de busca

--- app/src/main/java/com/arthurguedescaminha/testedesenvolvedorandroid/MainActivity.kt
+++ app/src/main/java/com/arthurguedescaminha/testedesenvolvedorandroid/MainActivity.kt
@@ -9,12 +9,14 @@ class MainActivity : AppCompatActivity() {
 
     val model: CardViewModel by viewModels()
 
+    lateinit var searchView: SearchView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         model.getCards().observe(this) { cards ->
-            if(cards.isEmpty()) {
+            if (cards.isEmpty()) {
                 val builder = AlertDialog.Builder(this)
                 builder.setMessage("Não foi possível encontrar nenhum card no banco de dados. Sua internet pode estar indisponível, o aplicativo será encerrado. Verifique a rede e tente novamente.")
                     .setPositiveButton("Ok") { dialog, id ->
@@ -30,6 +32,23 @@ class MainActivity : AppCompatActivity() {
 
     fun initUi() {
         model.getCards("teste").observe(this) { cards ->
+        searchView = findViewById(R.id.search_view)
+        searchView.isActivated = true
+        searchView.queryHint = "Digite aqui o nome do card"
+        searchView.onActionViewExpanded()
+        searchView.isIconified = false
+        searchView.clearFocus()
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                model.updateCardsList(newText)
+
+                return false
+            }
+        })
         }
     }
 
